29a223476cc67e4c79aa9e82ddfc348960992221
cd <lesleyrotonto@gmail.com>
2024-07-23 23:55:15 -0400

    update server it is running

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
 import './config/database.js'
+import 'dotenv/config.js'
 
+import express from 'express'
+import cors from 'cors'
+
+//import routes when they are created
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(cors())
+
+//once routes built put them here for the app
+
+app.listen(PORT, function(){
+    console.log(`App running on ${PORT} port`)
+})
